ac636ba8a02e7f4acd1010595d27f285c2bbf6b5
Oussama <oussama.melki@betavib.com>
2017-09-18 16:19:16 -0400

    sync3

--- TestProj/Form1.Designer.cs
+++ TestProj/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.btn3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button13 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOussama
@@ -92,12 +93,23 @@
             this.button13.Text = "SyncTest 2";
             this.button13.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(394, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Ouss 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(849, 393);
             this.Controls.Add(this.button13);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btn3);
             this.Controls.Add(this.label1);
@@ -117,6 +129,7 @@
         private System.Windows.Forms.Button btn3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button3;
     }
 }
 
